89464db143ab9397773de2d2d0edca2e38dcc5bb
André Quintiliano Filho <drequintiliano@gmail.com>
2020-07-20 00:06:08 -0300

    correções modal, efeito deslizante

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function($){
+    var $doc = $('html,body');
+    $('.scroll-page').click(function(){
+        $doc.animate({
+            scrollTop: $($.attr(this,'href')).offset().top
+        },500);
+        return false;
+    });
+})(jQuery);
